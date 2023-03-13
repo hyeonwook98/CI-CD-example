c8552fdc6d7d2d1f6a96128bdeb04b80cbf92869
hyeonwook <hyeonwook98@naver.com>
2023-03-13 21:54:18 +0900

    chore: CI/CD 예제 프로젝트 생성

---
+++ example/src/test/java/CD/CD/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package CD.CD.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
