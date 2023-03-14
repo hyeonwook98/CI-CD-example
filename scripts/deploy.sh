269b697a5f77aa4c852e38f073c806fee513da95
hyeonwook <hyeonwook98@naver.com>
2023-03-14 22:07:58 +0900

    fix: 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/app/deploy
 cd $REPOSITORY
 
-APP_NAME=deploy
+APP_NAME=DevOps-study
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
