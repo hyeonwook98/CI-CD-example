b887fc0fa7111982eb7c0b9024d0164b55b81d81
hyeonwook <hyeonwook98@naver.com>
2023-03-14 20:39:35 +0900

    fix: 코드 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@
 REPOSITORY=/home/ec2-user/app/deploy
 cd $REPOSITORY
 
-APP_NAME=cicdproject
+APP_NAME=deploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
